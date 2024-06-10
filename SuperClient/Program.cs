2d3e9d67854c1215b4502077c5fc1f292fa908b9
oddonenso <oddonenso@gmail.com>
2024-06-10 14:56:41 +0700

    клиент-сервер

---
+++ SuperClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        UdpClient clinet = new UdpClient(12345);
+        IPEndPoint iPEnd = new IPEndPoint(IPAddress.Any, 12345);
+
+        while (true)
+        {
+            byte[] data = clinet.Receive(ref iPEnd);
+            string receivedTime = Encoding.UTF8.GetString(data);
+            Console.WriteLine($"А на часах: {receivedTime}");
+        }
+    }
+}
---
+++ SuperServer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        UdpClient udpClient = new UdpClient();
+        IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Broadcast, 12345);
+
+        while (true)
+        {
+            string superTime = DateTime.Now.ToString("HH:mm:ss");
+            string message = superTime;
+
+            //проверка на звонок
+            if (DateTime.Now.Minute == 00 && DateTime.Now.Second == 0)
+            {
+                message += " - Звонок";
+            }
+
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            udpClient.Send(data, data.Length, iPEndPoint);
+            Thread.Sleep(1000);
+        }
+    }
+}
